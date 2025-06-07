236c2b018be728c9492c09f40f7d9c6e51b7ccb1
YULIYA2929 <138712567+YULIYA2929@users.noreply.github.com>
2025-06-07 21:12:01 +0300

    js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,28 +1,41 @@
-const form = document.querySelector('.feedback-form');
-const input = document.querySelector('input');
-const textArea = document.querySelector('textarea')
-
-// localStorage.clear()
-const storageKey = "feedback-form-state";
+const form = document.querySelector(".feedback-form");
+const emailInput = form.elements.email;
+const messageTextarea = form.elements.message;
+const localStorageKey = "feedback-form-state";
 
 const formData = {
-    email: '',
-    message: '',
+  email: "",
+  message: "",
+};
+
+const savedData = JSON.parse(localStorage.getItem(localStorageKey));
+if (savedData) {
+  formData.email = savedData.email ?? "";
+  formData.message = savedData.message ?? "";
+  emailInput.value = formData.email;
+  messageTextarea.value = formData.message;
 }
 
-form.addEventListener('submit', handleForm);
-form.addEventListener('input', handleInputForm);
+form.addEventListener("input", () => {
+  formData.email = emailInput.value.trim();
+  formData.message = messageTextarea.value.trim();
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
+});
 
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
 
+  if (!formData.email || !formData.message) {
+    alert("Fill please all fields");
+    return;
+  }
 
-const valueStorage = localStorage.getItem(storageKey);
-const parc = JSON.parse(valueStorage);
+  console.log(formData);
 
-if(parc !== null && parc !== undefined){
-    input.value = parc.email;
-    textArea.value = parc.message;
-    formData.email = parc.email;
-    formData.message = parc.message;
-    
-    
-}
+  localStorage.removeItem(localStorageKey);
+  form.reset();
+
+  formData.email = "";
+  formData.message = "";
+});
+
